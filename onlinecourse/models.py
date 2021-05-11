1301c999f862285deef338241d38e9c000631122
Ryan G <rmgraves@gmail.com>
2021-05-11 00:41:16 +0000

    more changes!

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -75,6 +75,9 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
@@ -111,14 +114,17 @@ class Question(models.Model):
 
     courses = models.ManyToManyField(Course)
 
+    def __str__(self):
+        return self.questionText
+
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 
 #  <HINT> Create a Choice Model with:
@@ -128,10 +134,13 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    choiceText = models.TextField()
+    choice_text = models.TextField()
     question = models.ManyToManyField(Question)
-    isCorrect = models.BooleanField()
+    is_correct = models.BooleanField()
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
 
 
 # <HINT> The submission model
